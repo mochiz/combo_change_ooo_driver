cc3f9b45463331327e76d805aaaed206b686f7d9
Hiroshi Asakura <h-asakura@smalltalk.co.jp>
2011-05-06 15:21:59 +0900

    変身音ファイルチェックと変身音再生処理をメソッド化した。

--- Resources/windows/combo_change.js
+++ Resources/windows/combo_change.js
@@ -1,9 +1,6 @@
 var win = Titanium.UI.currentWindow;
 
-// createSound, createImageView
-var sound = Titanium.Media.createSound({
-    url:'../sounds/' + win.combo + '.mp3'
-});
+// createImageView, createAnimation, createSound
 var belt = Titanium.UI.createImageView({
 	image:'../images/belt.png',
     width: 400,
@@ -12,23 +9,27 @@ var driver = Titanium.UI.createImageView({
 	image:'../images/' + win.combo + '.png',
     width: 400,
 });
-
-// createAnimation
 var anime = Titanium.UI.createAnimation({
     transform: Ti.UI.create2DMatrix({rotate: 28}),
     duration: 200,
+});
+var sound = Titanium.Media.createSound({
+    url:'../sounds/' + win.combo + '.mp3',
+    file_exists_and_play: function() {
+        f = Ti.Filesystem.getFile('sounds/' + win.combo + 'hoge.mp3');
+        if (!f.exists()) {
+            alert('歌（がないの）は気にするな！')
+            return;
+        }
+        sound.play();
+    }
 });
 
 // addEventListener
 driver.addEventListener('click', function()
 {
-    var f = Titanium.Filesystem.getFile('sounds/' + win.combo + '.mp3');
-    if (f.exists()) {
-        sound.play();
-    } else {
-        alert('歌（がないの）は気にするな！');
-    }
     driver.animate(anime);
+    sound.file_exists_and_play();
 });
 
 // win.add
