a5c252b0f30d9a78232dfdee412cc8d2b82c35f5
Hiroshi Asakura <mochi.hiro43@gmail.com>
2011-05-20 01:30:08 +0900

    Revert "UIプロパティ定義を別ファイルへ抽出した。" 実機でアプリが落ちるようになったので、一旦revertします。 This reverts commit d4226b9af9ff7a59df95cb7fd9fc3c459b08176d.

--- Resources/windows/combo_change.js
+++ Resources/windows/combo_change.js
@@ -1,35 +1,135 @@
 var win = Titanium.UI.currentWindow;
+
 win.orientationModes = [Titanium.UI.LANDSCAPE_LEFT];
 win.title = win.combo;
 
-// 変身音オブジェクト生成と音声ファイルのプリロード
-henshin_sound_components = require("lib/henshin_sound_components");
-var tatoba_sound = henshin_sound_components.tatoba_sound;
+// createImageView, createAnimation, createSound
+var belt = Titanium.UI.createImageView({
+    image:'../images/belt.png',
+    width: 480,
+});
+var driver = Titanium.UI.createImageView({
+    image:'../images/' + win.combo + '.png',
+    width: 480,
+    changed: false,
+});
+var cover = Ti.UI.createImageView({
+    image:'../images/cover.png',
+    width: 480,
+});
+var anime = Titanium.UI.createAnimation({
+    transform: Ti.UI.create2DMatrix({rotate: 28}),
+    duration: 200,
+});
+
+// オースキャナーオブジェクトの生成
+var o_scanner = Ti.UI.createImageView({
+    top: -80,
+    left: -70,
+    height: 200,
+    width: 200,
+    image:'../images/o_scanner.png',
+    visible: false,
+    vanish: function() {
+        this.animate(Ti.UI.createAnimation({
+            top: 350,
+            left: 600,
+            duration: 2000,
+        }));
+    },
+});
+
+// 変身音オブジェクトの生成と音声ファイルのプリロード
+var tatoba_sound = Titanium.Media.createSound({
+    url:'../sounds/' + win.combo + '.mp3',
+    preload:true,
+    has_sound_file: function() {
+        f = Ti.Filesystem.getFile('sounds/' + win.combo + '.mp3');
+        return f.exists();
+    },
+    file_exists_and_preload: function() {
+        if (this.has_sound_file()){
+            this.play();
+            this.pause();
+        }
+    },
+    file_exists_and_play: function() {
+        if (!this.has_sound_file()){
+            return;
+        }
+        this.play();
+    },
+});
 tatoba_sound.file_exists_and_preload();
-var raise_sound = henshin_sound_components.raise_sound;
+
+var raise_sound = Titanium.Media.createSound({
+    url:'../sounds/driver_raise.mp3',
+    preload:true,
+    preload: function() {
+        this.play();
+        this.pause();
+    },
+});
+var charging_sound = Titanium.Media.createSound({
+    url:'../sounds/charging.mp3',
+    preload:true,
+    looping:true,
+    preload: function() {
+        this.play();
+        this.pause();
+    },
+});
+var core_slash_sound = Titanium.Media.createSound({
+    url:'../sounds/core_slash.mp3',
+    preload:true,
+    preload: function() {
+        this.play();
+        this.pause();
+    },
+});
 raise_sound.preload();
-var charging_sound = henshin_sound_components.charging_sound;
 charging_sound.preload();
+core_slash_sound.preload();
 
-// オーズドライバーを構成オブジェクト生成
-driver_components = require("lib/driver_components");
-var belt = driver_components.belt;
-var driver = driver_components.driver;
-var cover = driver_components.cover;
-var anime = driver_components.anime;
-var o_scanner = driver_components.o_scanner;
-
-// コアメダルフラッシュ、オーラングサークルエフェクトオブジェクト生成
-var core_flash = require("lib/effect_core_flash").core_flash();
-var orangu_circle = require("lib/effect_orangu_circle").orangu_circle;
-
-// ライダー詳細webページオブジェクト生成
-rider_web = require("lib/rider_web_page_components");
-var webview = rider_web.webview;
-var scrollview = rider_web.scrollview;
+// ライダー詳細webページ
+var webview = Ti.UI.createWebView({
+    url: 'http://www.tv-asahi.co.jp/ooo/rider/' + win.combo + '.html',
+});
+var scrollview = Titanium.UI.createScrollView({
+    contentWidth:'auto',
+    contentHeight:1000,
+    showVerticalScrollIndicator:true,
+    showHorizontalScrollIndicator:true,
+    transform: Ti.UI.create2DMatrix().scale(0),
+    visible: false,
+    show_rider: function() {
+        this.show();
+        this.animate({
+            transform: Ti.UI.create2DMatrix(),
+            duration: 300,
+        });
+        win.setToolbar([flexSpace, brf_button, flexSpace]);
+    },
+});
 scrollview.add(webview);
-var brf_button = rider_web.brf_button;
-var flexSpace = rider_web.flexSpace;
+
+// back, reload, forward button_bar
+var brf_button = Titanium.UI.createButtonBar({
+    labels:['Back', 'Reload', 'Forward'],
+    backgroundColor:'black'
+});
+var flexSpace = Ti.UI.createButton({
+    systemButton:Ti.UI.iPhone.SystemButton.FLEXIBLE_SPACE
+});
+brf_button.addEventListener('click',function(ce) {
+    if (ce.index == 0) {
+        webview.goBack();
+    } else if (ce.index == 1) {
+        webview.reload();
+    } else {
+        webview.goForward();
+    }
+});
 
 // addEventListener
 webview.addEventListener('load', function()
@@ -135,6 +235,72 @@ tatoba_sound.addEventListener('complete', function()
     orangu_circle.vanish();
     scrollview.scrollTo(0, 225);
     setTimeout(function() { scrollview.show_rider() }, 300);
+});
+
+// core_flash!
+var flash_positions = [
+    {top: 60, left: 85},
+    {top: 105, left: 'auto'},
+    {top: 165, left: 300},
+];
+var core_flash = new Array();
+var vanish_anime = Ti.UI.createAnimation({opacity:0, duration:1500});
+for(var i=0;i<=2;i++) {
+    core_flash[i] = ['', ''];
+    for (var j=0;j<=1;j++) {
+        core_flash[i][j] = Ti.UI.createView({
+            height:90,
+            width:90,
+            borderRadius:45,
+            visible: false,
+            borderColor:win.color[i],
+            top: flash_positions[i].top,
+            left: flash_positions[i].left,
+            borderWidth:3,
+            opacity:0.6,
+            flash: function() {
+                this.show();
+                this.animate({
+                    transform: Ti.UI.create2DMatrix({scale: this.scale}),
+                    zIndex: 1,
+                    duration: 500,
+                });
+            },
+            vanish: function() {
+                this.animate(vanish_anime);
+            },
+        });
+    }
+    core_flash[i][0].scale = 2;
+    core_flash[i][1].scale = 2.2;
+    core_flash[i][0].sound = Titanium.Media.createSound({
+        url:'../sounds/medal_scan' + i + '.mp3',
+    });
+    core_flash[i][0].sound.play();
+    core_flash[i][0].sound.pause();
+}
+
+// オーラングサークルオブジェクトの生成
+var orangu_circle = Ti.UI.createImageView({
+    width: 200,
+    image:'../images/orangu_' + win.combo + '.png',
+    opacity:0.7,
+    visible: false,
+    flash: function() {
+        this.show();
+        this.animate({
+            transform: Ti.UI.create2DMatrix({scale: 1.6}),
+            zIndex: 1,
+            duration: 1000,
+        });
+    },
+    vanish: function() {
+        this.animate({
+            transform: Ti.UI.create2DMatrix({scale: 0.01}),
+            zIndex: 1,
+            duration: 500,
+        });
+    },
 });
 
 // win.add
